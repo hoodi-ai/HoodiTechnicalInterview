3d669c611dd61cd244ffd807c97e12af1d98e37a
alexhoodiai <alex@hoodi.ai>
2023-07-24 09:01:30 -0400

    Add algo

---
+++ algo/algo.ts
@@ -0,0 +1,16 @@
+// Algo qui prend en input un array de nombres (numbers) et un nombre cible (target). L'algo doit retourner 2 nombres qui additionnés ensemble donne la cible.
+
+// ex. 1:
+// inputs: numbers = [2, 7, 11, 15], target = 9
+// output: [2, 7]
+
+// ex. 2:
+// inputs: numbers = [4, 2, 9, 3], target = 5
+// output: [2, 3]
+
+// Exemple de fonction typescript: 
+
+function FindTwoNumbersThatSumsToTarget(numbers: number[], target: number): number[]
+{
+
+}
